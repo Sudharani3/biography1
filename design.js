2ee450244f43cb6fd16cf2ecdde16322bfea4cfc
KUNAPAREDDY SUDHA RANI <126180396+Sudharani3@users.noreply.github.com>
2023-07-28 19:38:14 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,45 @@
+ReactDOM.render(
+    
+  <div>
+     <h1>BIOGRAPHY OF KANE WILLIAMSON</h1>
+     <img src="https://img.cricketnmore.com/uploads/2021/07/kane-williamson-interesting-trivia-facts-and-records-image.jpg" height="300" width="500"/>
+    <p>
+     
+        <b><i>Full name</i></b>	:Kane Stuart Williamson<br/><br/> 
+        <b><i>Born</i></b>	        :8 August 1990 (age 32)<br/><br/> 
+        <b><i>place </i></b>       :Tauranga, New Zealand<br/><br/> 
+        <b><i>Height	</i></b>    :5 ft 8 in (1.73 m)<br/><br/>
+        <b><i>Batting	</i></b>    :Right-handed <br/><br/> 
+        <b> <i>Bowling</i>	</b>    :Right-arm off-break<br/><br/> 
+        <b><i>Role	</i></b>        :Top-order batter<br/><br/> 
+     </p>
+     <h3><u>International Career</u></h3>
+     <p>
+     Williamson was 17, when he led the New Zealand Under-19 side in the World Cup in Malaysia in 2008.<br/><br/>
+     Williamson made his One-Day International debut against India on 10 August 2010. <br/><br/>
+     Williamson made his Test cricket debut against India at Ahmedabad on 4 November 2010.<br/><br/>
+     In his first innings he scored 131 runs off 299 balls and became the eighth New Zealand player to score a century on Test debut.<br/><br/>
+     </p>
+     <h3><u>Captaincy</u></h3>
+     <p>
+     In March 2016, Williamson assumed the position of captain of New Zealand across all forms of cricket after the<br/><br/>
+     retirement of Brendon McCullum, beginning with the World T20I cup in India<br/><br/>
+     In December 2022, Williamson stepped down as New Zealand's Test captain, ahead of their tour to Pakistan.<br/><br/>
+     </p>
+     <h3><u>International Centuries</u></h3>
+     <p>
+     As of March 2023, Williamson has scored 28 Test and 13 ODI centuries. His highest score in Test is 251 and 148 in ODIs.<br/><br/>
+     He is yet to score a century in T20Is.[79]<br/><br/>
+     </p>
+     <h3><u>Achievements</u></h3>
+     <p>
+     In the annual ICC Awards in January 2022, Williamson was included in ICC Men's Test Team of the Year for the year 2021.<br/><br/>
+     </p>
+     <h3><u>Personal Life</u></h3>
+     <p>
+     He bowls and bats right handed but writes left handed.Williamson has two children, a daughter and son with<br/><br/>
+     wife Sarah Raheem whom he met in 2015.<br/><br/>
+     </p>
+    </div>
+     
+,document.getElementById("bio"));
